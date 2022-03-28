1a0a9a71870ff08358a9830a666d183bbb3986b5
Jens Schmitz <jensschmitz1@hotmail.com>
2022-03-28 21:39:51 +0200

    Add Problem 6

--- ProjectEuler/ProjectEuler/Problems/Problem2.cs
+++ ProjectEuler/ProjectEuler/Problems/Problem2.cs
@@ -13,6 +13,7 @@ namespace ProjectEuler.Problems
 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ...
 
 By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.";
+
         public override string Solve()
         {
             var sum = 0;
---
+++ ProjectEuler/ProjectEuler/Problems/Problem6.cs
@@ -0,0 +1,34 @@
+﻿using ProjectEuler.Problems.Interfaces;
+using System.ComponentModel.Composition;
+
+namespace ProjectEuler.Problems
+{
+    [Export(typeof(IProblem))]
+    internal class Problem6 : Problem
+    {
+        public override string Name => "Sum square difference";
+
+        public override string Description => @"The sum of the squares of the first ten natural numbers is, 1^2 + 2^2 + ... + 10^2 = 385
+
+The square of the sum of the first ten natural numbers is, (1 + 2 + ... + 10)^2 = 55^2 = 3025
+
+Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 - 385 = 2640.
+
+Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.";
+
+        public override string Solve()
+        {
+            double sum = 0;
+            var square = 0;
+            for (var i = 1; i <= 100; i++)
+            {
+                sum += Math.Pow(i, 2);
+                square += i;
+            }
+
+            var result = Math.Pow(square, 2) - sum;
+
+            return result.ToString();
+        }
+    }
+}
