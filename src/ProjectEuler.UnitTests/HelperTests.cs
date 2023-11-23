c43667dfdd27c310026e9446581aec08bc335e02
Jens Schmitz <jensschmitz1@hotmail.com>
2023-11-23 21:13:00 +0100

    switch to xunit and add coverage

--- src/ProjectEuler.UnitTests/HelperTests.cs
+++ src/ProjectEuler.UnitTests/HelperTests.cs
@@ -1,53 +1,53 @@
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions;
+using Xunit;
 
 namespace ProjectEuler.UnitTests
 {
-    [TestClass]
     public class HelperTests
     {
-        [TestMethod]
+        [Fact]
         public void IsPalindrome_Success()
         {
             AssertIsPalindrome(1331, true);
         }
 
-        [TestMethod]
+        [Fact]
         public void IsPalindrome_Fail()
         {
             AssertIsPalindrome(1337, false);
         }
 
-        [TestMethod]
+        [Fact]
         public void IsPrime_Success()
         {
             AssertIsPrime(48527, true);
         }
 
-        [TestMethod]
+        [Fact]
         public void IsPrime_Fail()
         {
             AssertIsPrime(48521, false);
         }
 
-        [TestMethod]
+        [Fact]
         public void IsPythagoreanTriplet_Success()
         {
             AssertIsPythagoreanTriplet(3, 4, 5, true);
         }
 
-        [TestMethod]
+        [Fact]
         public void IsPythagoreanTriplet_Fail()
         {
             AssertIsPythagoreanTriplet(15, 16, 17, false);
         }
 
-        [TestMethod]
+        [Fact]
         public void GetDivisorsCount_Few()
         {
             AssertGetDivisorsCount(6, 4);
         }
 
-        [TestMethod]
+        [Fact]
         public void GetDivisorsCount_Many()
         {
             AssertGetDivisorsCount(28, 6);
@@ -56,25 +56,25 @@ namespace ProjectEuler.UnitTests
         private static void AssertIsPalindrome(int number, bool expectedResult)
         {
             bool actualResult = Helper.IsPalindrome(number);
-            Assert.AreEqual(actualResult, expectedResult);
+            actualResult.Should().Be(expectedResult);
         }
 
         private static void AssertIsPrime(int number, bool expectedResult)
         {
             bool actualResult = Helper.IsPrime(number);
-            Assert.AreEqual(actualResult, expectedResult);
+            actualResult.Should().Be(expectedResult);
         }
 
         private static void AssertIsPythagoreanTriplet(int a, int b, int c, bool expectedResult)
         {
             bool actualResult = Helper.IsPythagoreanTriplet(a, b, c);
-            Assert.AreEqual(actualResult, expectedResult);
+            actualResult.Should().Be(expectedResult);
         }
 
         private static void AssertGetDivisorsCount(int number, int expectedCount)
         {
             int actualCount = Helper.GetDivisorsCount(number);
-            Assert.AreEqual(actualCount, expectedCount);
+            actualCount.Should().Be(expectedCount);
         }
     }
 }
